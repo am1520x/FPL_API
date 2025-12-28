edb97fa8023afbcaa8d7508ad18264d9ff83cb9d
am1520x <am1520@ic.ac.uk>
2025-12-28 17:20:16 +0000

    Added a root endpoint so it doesn't give 404 error.

--- app/main.py
+++ app/main.py
@@ -5,3 +5,8 @@ from app.api.router import router
 app = FastAPI(title="FPL Entry API", version="0.1.0")
 
 app.include_router(router)
+
+@app.get("/")
+def root():
+    return {"name": "FPL Entry API", "docs": "/docs"}
+
