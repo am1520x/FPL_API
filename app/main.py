7404b65d4fdd1434a2c583a6d8f436e2f30f3a1d
am1520x <am1520@ic.ac.uk>
2026-01-07 18:42:10 +0000

    Added metadata to main

--- app/main.py
+++ app/main.py
@@ -2,7 +2,10 @@
 from fastapi import FastAPI
 from app.api.router import router
 
-app = FastAPI(title="FPL Entry API", version="0.1.0")
+app = FastAPI(title="FPL Entry API", description="An API to fetch and analyse Fantasy Premier League data.", version="1.0.0", contact={
+    "name": "FPL Entry API Support",
+    "url": "https://github.com/am1520x/FPL_API",
+})
 
 app.include_router(router)
 
